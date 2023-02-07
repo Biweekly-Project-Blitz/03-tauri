60161de5545d4f5bf13a3661f9e4080015c85d22
evansteelepdx <evan.steele@evansteele.net>
2023-02-06 20:46:50 -0800

    added basic component for listing images

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -3,10 +3,30 @@
     windows_subsystem = "windows"
 )]
 
+use std::process::Command;
+use std::str;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn greet() -> String {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", "{'error':'sorry not ready for windows yet'}"])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("docker image ls --format '{{json .}}'")
+            .output()
+            .expect("failed to execute process")
+    };
+
+    let hello = match str::from_utf8(&output.stdout) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+    format!("{}", hello)
 }
 
 fn main() {
--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -1,17 +1,16 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
+let docker_images_button;
+let docker_images_text;
 
-async function greet() {
+async function fetch_docker_images() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  docker_images_text.textContent = await invoke("greet", {});
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
+  docker_images_text = document.querySelector("#docker-images");
   document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
+    .querySelector("#docker-images-button")
+    .addEventListener("click", () => fetch_docker_images());
 });
