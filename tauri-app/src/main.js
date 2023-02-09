88f832a6c7399302bf52611d5e9bb033d799ac5a
evansteelepdx <evan.steele@evansteele.net>
2023-02-08 22:17:02 -0800

    added react framework

--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -1,16 +1,29 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let docker_images_button;
-let docker_images_text;
+let docker_images_text = document.querySelector("#docker-images");
 
 async function fetch_docker_images() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   docker_images_text.textContent = await invoke("greet", {});
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  docker_images_text = document.querySelector("#docker-images");
-  document
-    .querySelector("#docker-images-button")
-    .addEventListener("click", () => fetch_docker_images());
-});
+function LikeButton() {
+  const [liked, setLiked] = React.useState();
+
+  return React.createElement(
+    'button',
+    {
+      onClick: () => fetch_docker_images(),
+    },
+    'Get Docker images',
+    React.createElement(
+      'h1',
+      null,
+      'Welcome!',
+    ),
+  );
+}
+
+const rootNode = document.getElementById('docker-container-root');
+const root = ReactDOM.createRoot(rootNode);
+root.render(React.createElement(LikeButton));
